f9d7b39deac80ee76538443d63f9e4e2d9babe0d
Marcel Mikl <marcel.mikl@codecentric.de>
2020-07-21 14:38:37 +0200

    working prototyp

--- ebs_app/main.py
+++ ebs_app/main.py
@@ -1,16 +1,14 @@
 import os
 from typing import List
 from enum import Enum
-from datetime import timedelta
 
 import boto3
+import botocore
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import HTMLResponse, FileResponse
+from starlette.responses import FileResponse
 from cryptography.fernet import Fernet
-
-
 
 key = Fernet.generate_key()
 f = Fernet(key)
@@ -45,45 +43,46 @@ class Decision(BaseModel):
 
 @app.get("/need_decision", response_model=dict)
 def get_need_decision_images():
-    response = s3.list_objects(
-        Bucket=BUCKET,
-        Prefix='unclear/*',
-    )
+    response = s3.list_objects(Bucket=BUCKET, Prefix="unclear/",)
     if "Contents" not in response:
         return {}
     blobs = response["Contents"]
     return_blob = None
-    
+
     for blob in blobs:
-        if ".jpeg" in blob.name or ".jpg" in blob.name:
+        if ".jpeg" in blob["Key"] or ".jpg" in blob["Key"]:
             return_blob = blob
             break
     if not return_blob:
         return {}
-    url = s3.generate_presigned_url('get_object', Params = {'Bucket': BUCKET, 'Key': return_blob['Key']}, ExpiresIn = 10)
-    return {"url": url, "id": f.encrypt(str.encode(return_blob['Key'])).decode("utf-8")}
+    url = s3.generate_presigned_url(
+        "get_object", Params={"Bucket": BUCKET, "Key": return_blob["Key"]}, ExpiresIn=10
+    )
+    return {"url": url, "id": f.encrypt(str.encode(return_blob["Key"])).decode("utf-8")}
 
-"""
+
 @app.post("/make_decision")
 def make_decision(decision: Decision):
     token = decision.id.encode("utf-8")
-    decrypted_blob_name = f.decrypt(token)
-    blob = bucket.get_blob(decrypted_blob_name)
-    if not blob:
+    decrypted_blob_name = f.decrypt(token).decode("utf-8")
+    try:
+        s3.head_object(Bucket=BUCKET, Key=decrypted_blob_name)
+    except botocore.exceptions.ClientError as e:
         raise HTTPException(status_code=404, detail="Blob not found")
 
     value = decision.decision
-    _, name = os.path.split(blob.name)
+    _, name = os.path.split(decrypted_blob_name)
     new_blob_name = f"{value}_{name}"
-    bucket.rename_blob(blob, f"human_decided/{new_blob_name}")
+
+    s3.copy_object(
+        Bucket=BUCKET,
+        Key=f"human_decided/{new_blob_name}",
+        CopySource={"Bucket": BUCKET, "Key": decrypted_blob_name}
+    )
+    s3.delete_object(Bucket=BUCKET, Key=decrypted_blob_name)
     return 200
-"""
+
 
 @app.get("/.*", include_in_schema=False)
 def root():
     return FileResponse("./static/index.html")
-"""
-@app.get("/.*")
-def root():
-    return {"Hello": "World"}
-        #return FileResponse("./static/index.html")"""
